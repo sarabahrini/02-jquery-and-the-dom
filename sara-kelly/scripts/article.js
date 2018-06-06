49cc38e33325b1952daca7a75f1ffbea820dad32
Jane Smith <example@email.com>
2018-06-06 11:47:28 -0700

    completed a couple of TODOs and added Modules.css code

--- sara-kelly/scripts/article.js
+++ sara-kelly/scripts/article.js
@@ -3,22 +3,40 @@
 let articles = [];
 
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
+// RESPONSE: The purpose of the function is to create raw data objects. It is capitalized because it is a constructor function. 
+
 
 function Article (rawDataObj) {
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
+  //NOTE: this function doesn't return anything in the page yet. 
+  
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
+
 }
 
 Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // PUT YOUR RESPONSE HERE
+  // RESPONSE: allows us to modify the content/ element prior to inserting them back into the document. 
+  
 
   let $newArticle = $('article.template').clone();
+  $(this.publishedOn).removeClass('article-template');
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
   if (!this.publishedOn) $newArticle.addClass('draft');
+ 
+  $newArticle.attr('data-title', this.title);
   $newArticle.attr('data-category', this.category);
+  $newArticle.attr('data-author', this.author);
+  $newArticle.attr('data-authorUrl', this.authorUrl);
+  $newArticle.attr('data-publishedOn', this.publishedOn);
+  $newArticle.text(this.body);
 
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
@@ -27,6 +45,8 @@ Article.prototype.toHtml = function() {
       3. article title,
       4. article body, and
       5. publication date. */
+     
+
 
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
